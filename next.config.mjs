30f9a52cc2e2069111b7d9c81de526e722cf8270
André Antunes <andrem.ferraz@hotmail.com>
2024-04-14 18:34:29 -0300

    pkglock fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
 };
-
+module.exports = nextConfig
 export default nextConfig;
